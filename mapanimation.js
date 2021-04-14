8ceb3c70311c2da7671478fd3d3636f2250ea5fc
Ann Suh <54272261+cd2life@users.noreply.github.com>
2021-04-13 20:46:57 -0400

    Update mapanimation.js
    Add marker animation through Mapbox.

--- mapanimation.js
+++ mapanimation.js
@@ -1,49 +1,38 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-    [-71.093729, 42.359244],
-    [-71.094915, 42.360175],
-    [-71.0958, 42.360698],
-    [-71.099558, 42.362953],
-    [-71.103476, 42.365248],
-    [-71.106067, 42.366806],
-    [-71.108717, 42.368355],
-    [-71.110799, 42.369192],
-    [-71.113095, 42.370218],
-    [-71.115476, 42.372085],
-    [-71.117585, 42.373016],
-    [-71.118625, 42.374863],
-  ];
+// add your own access token
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2ZsZWUxOTkxIiwiYSI6ImNrbjltOWx0aTBhd2kydm9vaHcyaGxudmwifQ.WCMUH5FwJNEot8yKjDY4Vw';
   
-  // add your own access token
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZ2ZsZWUxOTkxIiwiYSI6ImNrbjltOWx0aTBhd2kydm9vaHcyaGxudmwifQ.WCMUH5FwJNEot8yKjDY4Vw';
-  
-  // This is the map instance
-  let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-71.104081, 42.365554],
-    zoom: 14,
-  });
-  
-  // add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
-  let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
-  
-  // counter here represents the index of the current bus stop
-  let counter = 0;
-  function move() {
-    // move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-    // use counter to access bus stops in the array busStops
-    // make sure you call move() after you increment the counter.
-    setTimeout(() => {
-      if (counter >= busStops.length) return;
-      marker.setLngLat(busStops[counter]);
-      counter++;
-      move();
-    }, 1000);
-  }
+// This is the map instance
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [0, 0],
+  zoom: 2
+});
 
+// add a marker
+var marker = new mapboxgl.Marker();
   
-  // Do not edit code past this point
-  if (typeof module !== 'undefined') {
+function animateMarker(timestamp) {
+  var radius = 20;
+  
+  // Update the data to a new position based on the animation timestamp. The
+  // divisor in the expression `timestamp / 1000` controls the animation speed.
+  marker.setLngLat([
+  Math.cos(timestamp / 1000) * radius,
+  Math.sin(timestamp / 1000) * radius
+  ]);
+  
+  // Ensure it's added to the map. This is safe to call if it's already added.
+  marker.addTo(map);
+  
+  // Request the next frame of the animation.
+  requestAnimationFrame(animateMarker);
+}
+  
+// Start the animation.
+requestAnimationFrame(animateMarker);
+
+// Do not edit code past this point
+if (typeof module !== 'undefined') {
     module.exports = { move };
-  }
+}
